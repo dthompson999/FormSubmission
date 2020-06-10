b4cc317bb9d7129fb2e37e66628fb9e4e6e8c817
dthompson999 <63125447+dthompson999@users.noreply.github.com>
2020-06-10 11:22:24 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("success")]
+        public IActionResult Success(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            else{
+                return View("Index");
+            }
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
